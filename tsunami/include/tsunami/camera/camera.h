223d9e8c05549b00e17df025f9e451e514ff5b6d
Rafapp <rpadiper@gmail.com>
2026-03-30 13:40:52 -0600

    .

---
+++ tsunami/include/tsunami/camera/camera.h
@@ -0,0 +1,19 @@
+#include "tsunami/scene/transform.h"
+
+class Camera {
+  public:
+	Camera();
+	~Camera() = default;
+
+	void setTransform(const Transform& transform);
+	Transform getTransform() const;
+
+  private:
+	Transform m_transform;
+    glm::vec3 m_target;
+    glm::vec3 m_position;
+    glm::vec3 m_up;
+    float m_fov;
+    float m_nearClip;
+    float m_farClip;
+};
