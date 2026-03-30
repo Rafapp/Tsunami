6fa11475a629355f846dd77fdf0ac94e59964108
Andrew Tate <98910049+tater-tot25@users.noreply.github.com>
2026-03-30 15:32:19 -0600

    Header Order Stuff
    - No Clue

--- tsunami/src/app/app.cpp
+++ tsunami/src/app/app.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 
+#define VOLK_IMPLEMENTATION
 #include "volk.h"
 #define VMA_IMPLEMENTATION
 #define VMA_STATIC_VULKAN_FUNCTIONS 0
 #define VMA_DYNAMIC_VULKAN_FUNCTIONS 1
+#include "vk_mem_alloc.h"
 #include "VkBootstrap.h"
 #include "slang.h"
-#include "vk_mem_alloc.h"
+#include "tsunami/app/app.h"
 
 #include "tsunami/app/app.h"
 
